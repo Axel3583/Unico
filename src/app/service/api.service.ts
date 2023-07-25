33725761bead7c913adda125a206fd99b3ffb55f
0aibo <0aibo@TECH-PO2151FY2.tech.aubay.com>
2023-07-25 21:38:49 +0200

    code smell review

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,10 +1,9 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/env.dev';
 import { userModel } from '../models/user.model'; 
-import { HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
